8802411b7f5e53d5e594b490a671543aca7ad2d9
John_L <john6.lq@gmail.com>
2019-12-04 11:17:42 +0800

    init repo

---
+++ app/src/main/java/com/github/john/tsputil/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.john.tsputil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
